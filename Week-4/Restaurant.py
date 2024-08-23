97bc70669c82881a0c8c9a54a4764abd16b2dcb2
PPchayutt <67070115@kmitl.ac.th>
2024-08-23 22:41:35 +0700

    Add files

---
+++ Week-4/Restaurant.py
@@ -0,0 +1,18 @@
+"""Restaurant"""
+def main(a, b, c, d):
+    """main"""
+    if a == b:
+        print(f"Yes")
+    elif a < b:
+        total = a + d
+        discount = total * (c / 100)
+        price_after_dis = total - discount
+        different = price_after_dis - a
+        different = abs(different)
+        if price_after_dis > a:
+            print(f"No {different:.3f}")
+        elif price_after_dis < a:
+            print(f"Yes {different:.3f}")
+        else:
+            print("Yes")
+main(float(input()), float(input()), float(input()), float(input()))
