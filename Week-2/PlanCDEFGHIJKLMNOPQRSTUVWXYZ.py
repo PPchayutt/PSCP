27a8cadcdb9334012ddb117a589003264e6c8d8d
PPchayutt <67070115@kmitl.ac.th>
2024-08-27 21:36:45 +0700

    Add files

--- Week-2/PlanCDEFGHIJKLMNOPQRSTUVWXYZ.py
+++ Week-2/PlanCDEFGHIJKLMNOPQRSTUVWXYZ.py
@@ -6,18 +6,20 @@ def main():
     num2 = float(input())
     num3 = float(input())
     if order == "Ascend":
+        # เรียงตัวเลขจากน้อยไปมาก
         if num1 > num2:
-            num1, num2 = num2, num1
+            num1, num2 = num2, num1  # สลับ num1 กับ num2 ถ้า num1 มากกว่า num2
         if num2 > num3:
-            num2, num3 = num3, num2
+            num2, num3 = num3, num2  # สลับ num2 กับ num3 ถ้า num2 มากกว่า num3
         if num1 > num2:
-            num1, num2 = num2, num1
+            num1, num2 = num2, num1  # ตรวจสอบอีกครั้งแล้วสลับ num1 กับ num2 ถ้าจำเป็น
     elif order == "Descend":
+        # เรียงตัวเลขจากมากไปน้อย
         if num1 < num2:
-            num1, num2 = num2, num1
+            num1, num2 = num2, num1  # สลับ num1 กับ num2 ถ้า num1 น้อยกว่า num2
         if num2 < num3:
-            num2, num3 = num3, num2
+            num2, num3 = num3, num2  # สลับ num2 กับ num3 ถ้า num2 น้อยกว่า num3
         if num1 < num2:
-            num1, num2 = num2, num1
+            num1, num2 = num2, num1  # ตรวจสอบอีกครั้งแล้วสลับ num1 กับ num2 ถ้าจำเป็น
     print(f"{num1:.2f}, {num2:.2f}, {num3:.2f}")
 main()
--- Week-4/Restaurant.py
+++ Week-4/Restaurant.py
@@ -1,18 +1,27 @@
 """Restaurant"""
 def main(a, b, c, d):
     """main"""
-    if a == b:
-        print("Yes")
+    if a >= b:
+        total = a + d
+        discount1 = a * (c / 100)
+        discount2 = total * (c / 100)
+        price_after_dis1 = a - discount1
+        price_after_dis2 = total - discount2
+        different1 = price_after_dis1 - a
+        different2 = price_after_dis2 - a
+        if abs(different1) > abs(different2):
+            print(f"No {abs(different1 - different2):.3f}")
+        else:
+            print("Yes")
     elif a < b:
         total = a + d
         discount = total * (c / 100)
         price_after_dis = total - discount
         different = price_after_dis - a
-        different = abs(different)
         if price_after_dis > a:
-            print(f"No {different:.3f}")
+            print(f"No {abs(different):.3f}")
         elif price_after_dis < a:
-            print(f"Yes {different:.3f}")
+            print(f"Yes {abs(different):.3f}")
         else:
             print("Yes")
 main(float(input()), float(input()), float(input()), float(input()))
