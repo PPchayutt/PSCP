3195a498e8b8e90164ef54d623136d9bfa7d6a65
PPchayutt <67070115@kmitl.ac.th>
2024-09-05 22:02:27 +0700

    Add file

--- Week-4/Inflation.py
+++ Week-4/Inflation.py
@@ -1,5 +1,9 @@
 """Inflation"""
 def main():
     """main"""
-
+    n = int(float(input()) * 100)
+    k = int(input())
+    for _ in range(k):
+        n += n*381//10000
+    print(f"{n // 100}.{n % 100:>02}")
 main()
