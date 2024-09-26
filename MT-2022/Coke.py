a2e2f42528e8f75aaa62c872f0b5bd83fae760d5
PPchayutt <67070115@kmitl.ac.th>
2024-09-26 20:03:41 +0700

    Add files
    Add a lot of files

---
+++ MT-2022/Coke.py
@@ -0,0 +1,11 @@
+"""Coke"""
+def total(oldprice, exchange, newprice, want):
+    """total"""
+    price = oldprice * want
+    if not exchange:
+        print(price)
+    else:
+        discount = int((want - 1) / exchange)
+        price = (discount * newprice) + ((want - discount) * oldprice)
+        print(price)
+total(int(input()), int(input()), int(input()), int(input()))
