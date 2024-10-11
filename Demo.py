71d57a2d260b011aa7e063641c99ec313baeff12
PPchayutt <67070115@kmitl.ac.th>
2024-10-11 22:55:28 +0700

    Add

--- Demo.py
+++ Demo.py
@@ -1,39 +1,9 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Ui_Dialog(object):
-	def setupUi(self, Dialog):
-		Dialog.setObjectName("Dialog")
-		Dialog.resize(400, 300)
-
-		self.pushButton = QtWidgets.QPushButton(Dialog)
-		self.pushButton.setGeometry(QtCore.QRect(150, 70, 93, 28))
-
-		self.label = QtWidgets.QLabel(Dialog)
-		self.label.setGeometry(QtCore.QRect(130, 149, 151, 31))
-		self.label.setText("")
-
-		self.retranslateUi(Dialog)
-		QtCore.QMetaObject.connectSlotsByName(Dialog)
-		
-		# adding signal and slot
-		self.pushButton.clicked.connect(self.showmsg) 
-
-	def retranslateUi(self, Dialog):
-		_translate = QtCore.QCoreApplication.translate
-		Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-		self.pushButton.setText(_translate("Dialog", "Click"))
-		
-	def showmsg(self):
-		# slot
-		self.label.setText("You clicked me")
-
-if __name__ == "__main__":
-	app = QtWidgets.QApplication(sys.argv)
-
-	MainWindow = QtWidgets.QMainWindow()
-	ui = Ui_Dialog()
-
-	ui.setupUi(MainWindow)
-	MainWindow.show()
-	sys.exit(app.exec_())
+"""Demo"""
+def pc_killer():
+    """Kill the CPU"""
+    num = 0
+    while num >= 0:
+        print(num)
+        print("Oh My Pc Æ Æ")
+        num += 1
+pc_killer()
