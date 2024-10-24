d09a2622b410490c6e1502b244d398d278f224ef
PPchayutt <67070115@kmitl.ac.th>
2024-10-24 20:21:54 +0700

    add

--- [Recommend]/VerticalHistogram.py
+++ [Recommend]/VerticalHistogram.py
@@ -1,1 +1,30 @@
 """VerticalHistogram"""
+def main():
+    """main"""
+    text = input()
+    alpa = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    result = {}
+    for alpha in alpa:
+        result[alpha] = 0
+        for char in text:
+            if char == alpha:
+                result[alpha] += 1
+    maximum = result[max(result, key=result.get)]
+    for count in range(maximum, 0, -1):
+        if count < 10:
+            print(f" {count} ", end='')
+        else:
+            print(f"{count} ", end='')
+        for alpha in alpa:
+            if result[alpha] >= count and result[alpha]:
+                print(" *", end='')
+            elif not result[alpha]:
+                continue
+            else:
+                print("  ", end='')
+        print()
+    print(" " * 3, end='')
+    for alpha in alpa:
+        if result[alpha]:
+            print(f" {alpha}", end='')
+main()
