19bdac939fd050a78de1c1e6f243c7f9dce31904
PPchayutt <67070115@kmitl.ac.th>
2024-10-24 20:21:35 +0700

    Add files

--- Week-12-13-14/Bus Seat.py
+++ Week-12-13-14/Bus Seat.py
@@ -1,1 +1,24 @@
 """Bus Seat"""
+def main():
+    """main"""
+    column = int(input())
+    rows = int(input())
+    number = int(input())
+    count = 0
+    check = column
+    for i in range(1,column+1):
+        if count == 2:
+            count = 0
+            print()
+        for _ in range(rows):
+            if check == number:
+                print('XX',end=" ")
+            else:
+                print(f'{check:>02}',end=" ")
+            check += column
+        if i < column:
+            print()
+        check = column
+        check -= i
+        count += 1
+main()
